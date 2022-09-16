17aa24a99fbf8df19aa131dcf3fcd2d7f4f5bf70
mourenZhu <1207743591@qq.com>
2022-09-16 10:51:05 +0800

    feat: 添加自定义yml配置文件支持
    添加自定义yml配置文件支持

---
+++ src/main/java/cn/zhumouren/poetryclub/config/YamlPropertySourceFactory.java
@@ -0,0 +1,30 @@
+package cn.zhumouren.poetryclub.config;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author mourenZhu
+ * @version 1.0
+ * @description todo
+ * @date 2022/9/15 22:29
+ **/
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+    @Override
+    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource)
+            throws IOException {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(encodedResource.getResource());
+
+        Properties properties = factory.getObject();
+
+        return new PropertiesPropertySource(encodedResource.getResource().getFilename(), properties);
+    }
+}
