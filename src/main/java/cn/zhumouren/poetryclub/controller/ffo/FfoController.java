596b77c4345bbfa36b8f654359722f3ba364dedc
mouren Zhu <1207743591@qq.com>
2022-10-12 16:56:36 +0800

    feat: ffo 基本功能
    完成ffo http层 基本功能

--- src/main/java/cn/zhumouren/poetryclub/controller/ffo/FfoController.java
+++ src/main/java/cn/zhumouren/poetryclub/controller/ffo/FfoController.java
@@ -1,15 +1,19 @@
 package cn.zhumouren.poetryclub.controller.ffo;
 
 import cn.zhumouren.poetryclub.bean.vo.FfoGameRoomReqVO;
+import cn.zhumouren.poetryclub.bean.vo.FfoGameRoomResVO;
 import cn.zhumouren.poetryclub.common.response.ResponseResult;
 import cn.zhumouren.poetryclub.service.FfoService;
 import cn.zhumouren.poetryclub.utils.SecurityContextUtil;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/user/games/ffo")
 public class FfoController {
 
     private final FfoService ffoService;
@@ -18,15 +22,25 @@ public class FfoController {
         this.ffoService = ffoService;
     }
 
-    @PostMapping("/games/ffo/room")
+    @PostMapping("/room")
     public ResponseResult<String> createGameRoom(FfoGameRoomReqVO roomReqVO) {
         return ffoService.userCreateGameRoom(
                 SecurityContextUtil.getUserEntity(), roomReqVO.getName(), roomReqVO.getFfoType());
     }
 
-    @PostMapping("/games/ffo/room/enter")
+    @PostMapping("/room/enter")
     public ResponseResult<Boolean> EnterGameRoom(String roomId) {
         return ffoService.userEnterGameRoom(SecurityContextUtil.getUserEntity(), roomId);
+    }
+
+    @PostMapping("/room/leave")
+    public ResponseResult<Boolean> leaveGameRoom() {
+        return ffoService.userLeaveGameRoom(SecurityContextUtil.getUserEntity());
+    }
+
+    @GetMapping("/")
+    public ResponseResult<Set<FfoGameRoomResVO>> listFfoGameRoom() {
+        return ffoService.listFfoGameRoom();
     }
 
 
