ba7beed561f591ca65b02e9864ceedb248c1a464
mouren Zhu <1207743591@qq.com>
2022-11-23 23:31:56 +0800

    feat: 增加参数top校验
    增加参数top校验

--- src/main/java/cn/zhumouren/poetryclub/controller/CommonWordController.java
+++ src/main/java/cn/zhumouren/poetryclub/controller/CommonWordController.java
@@ -5,6 +5,7 @@ import cn.zhumouren.poetryclub.bean.mapper.CommonWordMapper;
 import cn.zhumouren.poetryclub.bean.vo.CommonWordResVO;
 import cn.zhumouren.poetryclub.common.response.ResponseResult;
 import cn.zhumouren.poetryclub.service.CommonWordService;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,11 @@ public class CommonWordController {
     }
 
     @GetMapping("/{top}")
-    public ResponseResult<List<CommonWordResVO>> getCommonWordTop(@PathVariable("top") int top) {
+    public ResponseResult getCommonWordTop(
+            @PathVariable("top") int top) {
+        if (top < 1 || top > 200) {
+            return ResponseResult.failedWithMsg("只能查询 1 ~ 200");
+        }
         List<CommonWordEntity> commonWordEntities = commonWordService.topCommonWords(top);
         List<CommonWordResVO> commonWordResVOS = CommonWordMapper.INSTANCE.toCommonWordResVOList(commonWordEntities);
         return ResponseResult.success(commonWordResVOS);
