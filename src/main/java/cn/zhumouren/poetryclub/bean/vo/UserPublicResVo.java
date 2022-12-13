0a83b23d779eab48a39f6474cfdf00a20bac555a
mouren Zhu <1207743591@qq.com>
2022-12-13 16:11:00 +0800

    feat: 增加用户头像url
    增加用户头像url

--- src/main/java/cn/zhumouren/poetryclub/bean/vo/UserPublicResVo.java
+++ src/main/java/cn/zhumouren/poetryclub/bean/vo/UserPublicResVo.java
@@ -1,5 +1,7 @@
 package cn.zhumouren.poetryclub.bean.vo;
 
+import cn.zhumouren.poetryclub.util.UserUtil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -14,7 +16,18 @@ public class UserPublicResVo implements Serializable {
     @Length(message = "用户名不能为空", min = 6, max = 30)
     @NotEmpty
     private final String username;
+
+    @JsonIgnore
     private final String avatarName;
     @Length(max = 20)
     private final String nickname;
+
+    /**
+     * 这是实际的前端获取头像url的方法
+     *
+     * @return
+     */
+    public String getAvatar() {
+        return UserUtil.getUserAvatarUrl(avatarName);
+    }
 }
