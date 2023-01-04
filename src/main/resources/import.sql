3b7ed701716e3f6d3c2e40a426dbc3fb458b637b
mouren Zhu <1207743591@qq.com>
2023-01-04 14:49:07 +0800

    feat: 优化配置
    优化配置

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
+-- CREATE EXTENSION zhparser;
+-- CREATE TEXT SEARCH CONFIGURATION chinese_zh (PARSER = zhparser);
+-- ALTER TEXT SEARCH CONFIGURATION chinese_zh ADD MAPPING FOR n,v,a,i,e,l WITH simple;
 CREATE INDEX idx_gin_poem_entity_content ON poem_entity USING gin (to_tsvector('chinese_zh', content));
 
 CREATE INDEX idx_gin_poem_entity_title ON poem_entity USING gin (to_tsvector('chinese_zh', title));
